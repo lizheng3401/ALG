b7b2f412c6381ba91717cf33541776f12f4c55cf
李正 <liz@knownsec.com>
2018-06-13 17:37:05 +0800

    :pencil: 1007

--- sjtu/1007.cpp
+++ sjtu/1007.cpp
@@ -1,48 +1,39 @@
 #include <iostream>
 #include <cstdio>
-#include <iomanip>
 #include <cstring>
 
 using namespace std;
 
-void add(char x[], char y[]);
-int main()
-{
-    string x,y, x1, x2, y1, y2;
-    cin>>x>>y;
-
-    return 0;
-//    cout<<setiosflags(ios::fixed)<<setprecision(2)<<x+y<<" "<<z;
-}
-
-
 void add(char x[], char y[])
 {
-    int num[400] = {0};
-    int X[400] = {0}, Y[400] = {0};
+    int num[300] = {0};
+    char XL[101],XR[101],YL[100], YR[100];
     int lx = strlen(x), ly = strlen(y), length = max(lx, ly);
-
-    for (int i = lx - 1, j = 0; i >= 0; --i, ++j) {
-        if(x[i] != '.'){
-            X[j] = x[i] - 48;
-        }else{
-            X[j] = -1;
-        }
+    int flag = 0;
+    while(flag < lx)
+    {
+        if(x[flag] == '.')
+            break;
+        ++flag;
     }
-
-    for (int i = ly - 1, j = 0; i >= 0; --i, ++j) {
-        if(y[i] != '.'){
-            Y[j] = y[i] - 48;
-        }else{
-            Y[j] = -1;
-        }
-    }
-
-    for (int k = 0; k < length; ++k) {
-        if(X[k] == -1 | Y[k] == -1)
+    for (int i = lx - 1, j = 0; i >= 0 ; --i, ++j) {
+        if(flag < i)
         {
-            num[k] = -1;
+            XR[j] = x[i];
+        }else if( flag > i){
+            XL[j] = x[i];
+        }else{
+            j = 0;
         }
-        num[k] = X[k]+Y[k];
     }
+
+    cout<<"OK";
+
+}
+int main()
+{
+    char x[200], y[200];
+    cin>>x>>y;
+    add(x, y);
+    return 0;
 }
--- sjtu/1014.cpp
+++ sjtu/1014.cpp
@@ -1,4 +1,45 @@
-//
-// Created by ksuser on 18-6-12.
-//
+#include <iostream>
+#include <cstdio>
+#include <cstring>
 
+using namespace std;
+
+void add(char x[], char y[])
+{
+    int num[300] = {0};
+    int X[200], Y[200] = {0};
+    int lx = strlen(x), ly = strlen(y), length = max(lx, ly);
+    for (int i = lx - 1, j = 0; i >= 0 ; --i, ++j) {
+        X[j] = x[i] - '0';
+    }
+    for (int i = ly - 1, j = 0; i >= 0 ; --i, ++j) {
+        Y[j] = y[i] - '0';
+    }
+    for (int i = 0; i < length; ++i) {
+        num[i] += X[i] + Y[i];
+        num[i+1] += num[i] / 10;
+        num[i] = num[i] % 10;
+    }
+    int i = 300 -1;
+    while(i >= 0)
+    {
+        if(num[i] !=0)
+            break;
+        i--;
+    }
+    if( i == -1)
+        cout<<"0";
+    else{
+        for (int k = i; k >=0 ; --k) {
+            cout<<num[k];
+        }
+    }
+
+}
+int main()
+{
+    char x[200], y[200];
+    cin>>x>>y;
+//    add(x, y);
+    return 0;
+}
